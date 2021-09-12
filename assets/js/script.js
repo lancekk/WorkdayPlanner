7caadeb967d5718fd36bce741601ebc9f613d879
Lance Kelley <lancekkcodes@gmail.com>
2021-09-12 12:52:41 -0500

    update displayed time

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,11 @@ let timeblocks = $('.container');
 let dayDiv = $('#currentDay');
 const workHours = [9,10,11,12,1,2,3,4,5];
 
-const dayFormat = 'dddd, MMMM Do';
+const dayFormat = 'dddd, MMMM Do [at] h:mm:ss a';
 
-function showCurrentDay() {
+const showCurrentDay = function () {
   dayDiv.text(moment().format(dayFormat));
+  setTimeout(showCurrentDay, 1000);
 }
 
 
