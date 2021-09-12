ececa11061cbf4b421fa1a58bf3f03b4dd935018
Lance Kelley <lancekkcodes@gmail.com>
2021-09-12 12:46:23 -0500

    switch to jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-let timeblocks = document.querySelector('.container');
-let dayDiv = document.querySelector('#currentDay');
+let timeblocks = $('.container');
+let dayDiv = $('#currentDay');
 const workHours = [9,10,11,12,1,2,3,4,5];
 
 const dayFormat = 'dddd, MMMM Do';
 
 function showCurrentDay() {
-  dayDiv.textContent = moment().format(dayFormat);
+  dayDiv.text(moment().format(dayFormat));
 }
 
 
