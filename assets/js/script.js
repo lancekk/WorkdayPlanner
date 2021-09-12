590d954260a6171742720b35a60a0505e646c523
Lance Kelley <lancekkcodes@gmail.com>
2021-09-12 12:40:04 -0500

    display current day at top of page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,14 @@ let timeblocks = document.querySelector('.container');
 let dayDiv = document.querySelector('#currentDay');
 const workHours = [9,10,11,12,1,2,3,4,5];
 
+const dayFormat = 'dddd, MMMM Do';
+
+function showCurrentDay() {
+  dayDiv.textContent = moment().format(dayFormat);
+}
+
+
+
+
+
+showCurrentDay();
